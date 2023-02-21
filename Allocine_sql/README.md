785f9daf05df87c43c5992f896bd4a11122b7248
sammyremi <adebayorsamuelas@gmail.com>
2023-02-21 17:29:09 +0100

    Allocine with SQL

---
+++ Allocine_sql/README.md
@@ -0,0 +1,25 @@
+# Welcome to My Allocine
+Welcome to my Allocine project
+
+## Task
+The tasks is to write a structure query language SQL
+
+## Description
+We solved the problem by writing sql query that gets data from the database with the query instruction passed
+
+## Installation
+This database contains information about movies, directors, actors, genres, and reviews.
+
+## Usage
+sudo apt-get install mysql-server
+
+sudo apt-get update
+```
+./my_project argument1 argument2
+```
+
+### The Core Team
+
+
+<span><i>Made at <a href='https://qwasar.io'>Qwasar SV -- Software Engineering School</a></i></span>
+<span><img alt='Qwasar SV -- Software Engineering School's Logo' src='https://storage.googleapis.com/qwasar-public/qwasar-logo_50x50.png' width='20px'></span>
---
+++ Allocine_sql/ex00/my_allocine.rb
@@ -0,0 +1,49 @@
+require "sqlite3"
+
+requests['Display all actors'] = "SELECT * FROM actors";
+
+requests['Display all genres'] = "SELECT * from genres";
+
+requests['Display the name and year of the movies'] = "SELECT mov_title, mov_year FROM movies";
+
+requests['Display reviewer name from the reviews table'] = "SELECT rev_name FROM reviews"
+
+requests["Find the year when the movie American Beauty released"] = "SELECT mov_year From movies WHERE mov_title = 'American Beauty'"
+
+requests["Find the movie which was released in the year 1999"] = "SELECT mov_title FROM movies WHERE mov_year = '1999'"
+
+requests["Find the movie which was released before 1998"] = "SELECT mov_title FROM movies WHERE mov_year < '1998'"
+
+requests["Find the name of all reviewers who have rated 7 or more stars to their rating order by reviews rev_name (it might have duplicated names :-))"] = 
+"SELECT rev_name FROM reviews, movies_ratings_reviews WHERE movies_ratings_reviews.rev_id = reviews.id AND movies_ratings_reviews.rev_stars >= 7 AND reviews.rev_name IS NOT NULL ORDER BY rev_name"
+
+requests["Find the titles of the movies with ID 905, 907, 917"] = "SELECT mov_title FROM movies WHERE id IN (905,907,917)"
+
+requests["Find the list of those movies with year and ID which include the words Boogie Nights"] = "SELECT id, mov_title, mov_year FROM movies WHERE mov_title LIKE '%Boogie Nights%'"
+
+requests["Find the ID number for the actor whose first name is 'Woody' and the last name is 'Allen'"] = 
+"SELECT id FROM actors WHERE act_fname = 'Woody' AND act_lname = 'Allen'"
+
+requests["Find the actors with all information who played a role in the movies 'Annie Hall'"] = 
+"SELECT actors.id, actors.act_fname, actors.act_lname, actors.act_gender FROM actors JOIN movies_actors ON actors.id = movies_actors.act_id JOIN movies ON movies_actors.mov_id = movies.id WHERE movies.mov_title = 'Annie Hall'"
+
+requests["Find the first and last names of all the actors who were cast in the movies 'Annie Hall', and the roles they played in that production"] = 
+"SELECT actors.act_fname, actors.act_lname, movies_actors.role FROM actors 
+JOIN movies_actors ON actors.id = movies_actors.act_id 
+JOIN movies ON movies_actors.mov_id = movies.id WHERE movies.mov_title = 'Annie Hall'"
+
+requests["Find the name of movie and director who directed a movies that casted a role as Sean Maguire"] = 
+"SELECT dir_fname, dir_lname, mov_title
+FROM movies
+JOIN directors_movies ON movies.id = directors_movies.mov_id
+JOIN directors ON directors_movies.dir_id = directors.id
+JOIN movies_actors ON movies.id = movies_actors.mov_id
+JOIN actors ON movies_actors.act_id = actors.id
+WHERE role = 'Sean Maguire'"
+
+requests["Find all the actors who have not acted in any movie between 1990 and 2000 (select only actor first name, last name, movie title and release year)"] = 
+"SELECT actors.act_fname, actors.act_lname, movies.mov_title, movies.mov_year
+FROM actors 
+INNER JOIN movies_actors ON actors.id = movies_actors.act_id
+INNER JOIN movies ON movies_actors.mov_id = movies.id
+WHERE movies.mov_year NOT BETWEEN 1990 AND 2000"
